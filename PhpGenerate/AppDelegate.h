22fe8fc94a865e9be75273745a7a0b918fe3a5d4
Shawn Chan <chengchao0311@gmail.com>
2015-04-26 09:50:48 +0800

    Initial Commit

---
+++ PhpGenerate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhpGenerate
+//
+//  Created by Xinqi Chan on 4/26/15.
+//  Copyright (c) 2015 Xinqi Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
